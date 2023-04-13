61549132022220aaf3b8a5aea0aee2b7de11abb7
pony <ponycandy666@126.com>
2023-04-13 21:07:57 +0800

    missing include

---
+++ include/NMPC/NMPC_ODE.h
@@ -0,0 +1,14 @@
+﻿#ifndef NMPC_ODE_H
+#define NMPC_ODE_H
+
+#include <QObject>
+#include <QString>
+#include <Eigen/Core>
+class NMPC_ODE
+{
+public:
+    virtual ~NMPC_ODE(){}
+    virtual Eigen::MatrixXd ode_function(Eigen::MatrixXd act_mat,Eigen::MatrixXd state_mat)=0;
+};
+#endif 
+
