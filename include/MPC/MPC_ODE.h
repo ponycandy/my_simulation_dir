51da06280d2151feae32cda881befc0689ea6559
pony <ponycandy666@126.com>
2023-04-25 20:48:36 +0800

    补充头文件

---
+++ include/MPC/MPC_ODE.h
@@ -0,0 +1,17 @@
+﻿#ifndef MPC_ODE_H
+#define MPC_ODE_H
+
+#include <QObject>
+#include <QString>
+#include <Eigen/Core>
+class MPC_ODE
+{
+public:
+    virtual ~MPC_ODE(){}
+    virtual Eigen::MatrixXd ode_function(Eigen::MatrixXd act_mat,Eigen::MatrixXd state_mat)=0;
+    virtual void get_A_and_B(Eigen::MatrixXd act_mat,
+     Eigen::MatrixXd state_mat,Eigen::MatrixXd &A_mat,Eigen::MatrixXd &B_mat)=0;
+
+};
+#endif 
+
---
+++ include/MPC/NMPC_dynamic_Jacob.h
@@ -0,0 +1,14 @@
+﻿#ifndef NMPC_DYNAMIC_JACOB_H
+#define NMPC_DYNAMIC_JACOB_H
+
+#include <QObject>
+#include <QString>
+#include <Eigen/Core>
+class NMPC_Dynamic_Jacob
+{
+public:
+    virtual ~NMPC_Dynamic_Jacob(){}
+    virtual Eigen::MatrixXd jacobica(Eigen::MatrixXd act_mat,Eigen::MatrixXd state_mat)=0;
+};
+#endif 
+
