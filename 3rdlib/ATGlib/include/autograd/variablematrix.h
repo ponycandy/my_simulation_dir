fa06b91fa3f28b54fa0778a84e1ddd94ab1dc24e
JudgeLevia <ponycandy666@126.com>
2023-07-21 20:16:06 +0800

    补充一下漏的包

--- 3rdlib/ATGlib/include/autograd/variablematrix.h
+++ 3rdlib/ATGlib/include/autograd/variablematrix.h
@@ -14,12 +14,15 @@ class ATtensor
 public:
 	ATtensor();
 	~ATtensor();
+	ATtensor Transpose();
+	Eigen::MatrixXd toMat();
 	void resize(int nrows, int ncols);
 	void setvalue(double value);
 	int rows;
 	int cols;
 	int totalsize;
 	Atvariable_Ptr* operator[](int i);
+	const Atvariable_Ptr* operator[](int i) const;
 	std::vector<Atvariable_Ptr> data;
 	
 };
@@ -27,29 +30,31 @@ public:
 
 //Some important symbol overloading
 
-ATtensor operator*(ATtensor& a, double b);
+ATtensor operator*(const ATtensor& a, const double b);
 
-ATtensor operator*(double b, ATtensor& a);
-
-
-ATtensor operator/(ATtensor& a, double b);
+ATtensor operator*(const double b, const ATtensor& a);
 
 
-ATtensor operator*(ATtensor& b, ATtensor& a);
+ATtensor operator/(const ATtensor& a, const double b);
 
-ATtensor operator+(ATtensor& b, ATtensor& a);
+
+ATtensor operator*(const ATtensor& b, const ATtensor& a);
+
+ATtensor operator+(const ATtensor& b, const ATtensor& a);
 
 //Operation of constant matrix
-ATtensor operator*(ATtensor& b, Eigen::MatrixXd& a);
-ATtensor operator*(Eigen::MatrixXd& b ,ATtensor& a);
-ATtensor operator+(ATtensor& b, Eigen::MatrixXd& a);
-ATtensor operator+(Eigen::MatrixXd& b, ATtensor& a);
+ATtensor operator*(const ATtensor& b, const Eigen::MatrixXd& a);
+ATtensor operator*(const Eigen::MatrixXd& b , const ATtensor& a);
+ATtensor operator+(const ATtensor& b, const Eigen::MatrixXd& a);
+ATtensor operator+(const Eigen::MatrixXd& b, const ATtensor& a);
 //debugging tool
 
 void printTensor(ATtensor& a);
 
 //Jacobi solution
-void GetJacobian(ATtensor& leftside, ATtensor& upside, Eigen::MatrixXd& returnmat);
+namespace autograd {
+	void GetJacobian(ATtensor& leftside, ATtensor& upside, Eigen::MatrixXd& returnmat);
+}
 //void GetJacobian(ATGTensor& leftside, ATGTensor& upside, Eigen::MatrixXd& returnmat);
 
 #endif 
